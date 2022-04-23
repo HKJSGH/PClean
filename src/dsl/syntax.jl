3ea3bed14e7543e4b62eb3322698ca70f92586c8
HKJSGH <79822496+HKJSGH@users.noreply.github.com>
2022-04-23 12:57:10 +0800

    Update syntax.jl

--- src/dsl/syntax.jl
+++ src/dsl/syntax.jl
@@ -16,11 +16,12 @@ using MacroTools
 """
     strip_dots(expr)
 
-If expr is a dot expression, remove everything after the dot.
+If expr is a dot expression, remove everything after the dot. 
+strip_dots()函数会移除点表达式在 . 后面的所有内容
 """
 function strip_dots(expr)
     if expr isa Expr && expr.head == :.
-        return strip_dots(expr.args[1])
+        return strip_dots(expr.args[1]) # 递归调用strip_dots()函数，初始时把表达式可成 a . b,吗，每次检查 a 是否是 “ a . b ”的形式
     end
     return expr
 end
@@ -34,29 +35,39 @@ Resolves `expr` to return three things:
 * a list of symbols, of the same length as the list of PClean names, used by the modified
   expression.
 * a modified expression that uses a single symbol in the place of any PClean name.  
+
+解析' expr '返回三样东西:
+*所有PClean名称的列表(局部变量或以引用槽开头的点表达式)
+使用expr
+*被修改者使用的与PClean名称列表相同长度的符号列表
+表达式。
+一个修改的表达式，在任何PClean名称的位置使用一个符号。
+
 """
-function parse_compound_expression(var_names::Set{Symbol}, fk_names::Set{Symbol}, expr)
-    # A local variable
+function parse_compound_expression(var_names::Set{Symbol}, fk_names::Set{Symbol}, expr) # 定义域是符号的集合
+    # A local variable 一个局部变量
     if expr isa Symbol && in(expr, var_names)
         return [expr], [expr], esc(expr)
     end
   
-    # A dot expression
+    # A dot expression       一个 . 表达式
     if expr isa Expr && expr.head == :. && in(strip_dots(expr), fk_names)
         return [expr], [Symbol(expr)], esc(Symbol(expr))
     end
   
-    # A symbol meant to refer to some outside constant
+    # A symbol meant to refer to some outside constant     一个代表外部变量的符号
     if expr isa Symbol
         return [], Symbol[], esc(expr)
     end
   
-    # A literal or other non-expression type
+    # A literal or other non-expression type          文字或者其他非表达式类型
     if !(expr isa Expr)
         return [], Symbol[], expr
     end
     
-    # Otherwise, it's a compound expression that we need to process recursively.
+    # Otherwise, it's a compound expression that we need to process recursively. 
+    # 否则，它是我们需要递归处理的复合表达式，下文是递归调用的语句
+    # 从中我们也可以看出，return的依次是pclean_names，used_symbols，new_expression。
     recursive_calls = [parse_compound_expression(var_names, fk_names, a) for a in expr.args]
     pclean_names = unique(vcat([result[1] for result in recursive_calls]...))
     used_symbols = unique(vcat([result[2] for result in recursive_calls]...))
@@ -72,6 +83,12 @@ Returns either the expression itself (if it is already a PClean name), or
   * a list of PClean names
   * a list of Julia names
   * an expression that uses the Julia names for the values indicated by the PClean names
+    
+   返回表达式本身(如果它已经是一个PClean名称)，或者
+  *一个PClean名称列表
+  julia名字的列表 
+  一个使用Julia名称来表示PClean名称所指示的值的表达式
+    
 """
 function parse_expression(var_names::Set{Symbol}, fk_names::Set{Symbol}, expr)
     if expr isa Symbol && in(expr, var_names)
@@ -89,6 +106,10 @@ end
 # Helper function that replaces block expressions with 
 # literal :begin and :end lines, and brings all lines inside
 # blocks to the top level.
+    
+# 用于将 block表达式 替换为 文字 “：begin ” 和 “：end ” 行 的辅助函数
+# 并将 block 内的所有行带到顶层。
+    
 function flatten_lines(lines)
     flatlines = []
     for line in lines
